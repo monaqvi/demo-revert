6da8b9f4124fe1b7565288866be32c7c75d88c21
monaqvi <naqvi.muhammad@gmail.com>
2023-01-09 20:30:01 -0600

    add autocomplete fn

---
+++ index.js
@@ -0,0 +1,27 @@
+// Autocomplete widget
+$(function () {
+  var skillNames = [
+    'Bootstrap',
+    'C',
+    'C++',
+    'CSS',
+    'Express.js',
+    'Git',
+    'HTML',
+    'Java',
+    'JavaScript',
+    'jQuery',
+    'JSON',
+    'MySQL',
+    'Node.js',
+    'NoSQL',
+    'PHP',
+    'Python',
+    'React',
+    'Ruby',
+  ];
+  $('#skill-name').autocomplete({
+    source: skillNames,
+  });
+});
+
